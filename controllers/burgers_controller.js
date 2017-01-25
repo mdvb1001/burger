30a32999efa88398ee698a6615448e9e543ca7bd
maxvanbel <maxvanbel@gmail.com>
2017-01-24 21:57:18 -0600

    add api routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,6 +8,23 @@ module.exports = function(app) {
             });
         });
     });
+    app.get("/api", function(req, res) {
+        burgerFunctions.getAll(function(data) {
+            res.json(data);
+            if (data[0].devoured === 0) {
+                res.json("Devoured Burgers: " + data);
+            } else {
+                res.json("Uneaten Burgers: " + data);
+            }
+        });
+    });
+    app.get("/api/:id", function(req, res) {
+        burgerFunctions.getBurgerById(req.params.id, function(data){
+            res.json(data);
+        });
+    });
+
+
     app.post('/', function(req, res) {
         var newBurg = req.body.burger;
         if (newBurg !== "") {
@@ -17,5 +34,12 @@ module.exports = function(app) {
         } else {
             res.redirect('/');
         }
+    });
+    app.put('/:id', function(req, res) {
+        var selectBurg = req.params.id;
+        burgerFunctions.devourBurger(selectBurg, function() {
+            res.redirect('/');
+        });
+
     });
 };
--- models/burger.js
+++ models/burger.js
@@ -15,11 +15,23 @@ module.exports = { //
             callback(data);
         });
     },
-    addBurger: function (burger, callback) {
-        connectionSQL.query("INSERT INTO burgers (burger_name) VALUE (?)", [burger], function (err, data) {
+    getBurgerById: function (burgerId, callback) {
+        connectionSQL.query('SELECT * FROM burgers WHERE id = ?', [burgerId], function (err, data) {
+            if (err) throw err;
+            callback(data);
+        });
+    },
+    addBurger: function (input, callback) {
+        connectionSQL.query("INSERT INTO burgers (burger_name) VALUE (?)", [input], function (err, result) {
             if (err) throw err;
             callback();
         });
     },
-    devourBurger: function (callback) {}
+    devourBurger: function (burgerId, callback) {
+        connectionSQL.query("UPDATE burgers SET devoured = ? WHERE id = ?", [1, burgerId], function (err, result) {
+            if (err) throw err;
+            console.log('Result: ' + result);
+            callback();
+        });
+    }
 };
